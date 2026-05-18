21f388057b1166e80cff3c88d480f4463294353d
Luis Jair <jair.escalante@phs.mx>
2026-05-17 18:59:39 -0600

    feat: add docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    // TypeOrmModule.forRootAsync({
+    //   imports: [],
+    //   useFactory: (configService: ConfigService) => ({
+    //     dialect: 'mysql',
+    //     host: configService.get('HOST'),
+    //     port: +configService.get('PORT'),
+    //     username: configService.get('USERNAME'),
+    //     password: configService.get('PASSWORD'),
+    //     database: configService.get('DATABASE'),
+    //     models: [],
+    //   }),
+    // }),
   ],
   controllers: [AppController],
   providers: [],
