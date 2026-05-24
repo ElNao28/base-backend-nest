a9384ccd8f9f519b14722367635c1e2fb8d884da
Luis Jair <jair.escalante@phs.mx>
2026-05-24 02:30:20 -0600

    feat: add jwt configuration

--- src/configuration/configuration.module.ts
+++ src/configuration/configuration.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import databaseConfig from './config/database.config';
 import authConfiguration from './config/auth.config';
+import jwtConfiguration from './config/jwt.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [databaseConfig, authConfiguration],
+      load: [databaseConfig, authConfiguration, jwtConfiguration],
     }),
   ],
   exports: [ConfigModule],
