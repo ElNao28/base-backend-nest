861b9bc5c4abf17831411b5c9b44424e95be9b3f
Luis Jair <jair.escalante@phs.mx>
2026-05-25 00:39:43 -0600

    feat: add public decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './auth/decorators/public-decorator.decorator';
 
 @Controller()
 export class AppController {
   constructor() {}
 
+  @Public()
   @Get()
   getHello(): string {
     return 'Hello world';
